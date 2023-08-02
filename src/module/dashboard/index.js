2c62bf9ab3c5c2843df8c3639e362f54e52cf5a6
isnasmkn2 <isnaputra123@gmail.com>
2023-08-02 11:07:34 +0700

    first commit

---
+++ src/module/dashboard/index.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+
+
+export default function DashboardPage(){
+  return (
+    <>
+      <Layout title='Dashboard Page'>
+        <main>
+          <p>Dashboard Page</p>
+        </main>
+      </Layout>
+    </>
+  )
+}
